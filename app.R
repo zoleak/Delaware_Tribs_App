fbf6a1b8fbb7b12afdee2c9d2e7c1b76951c7957
zoleak <36116239+zoleak@users.noreply.github.com>
2019-07-11 14:48:26 -0400

    Add files via upload

--- app.R
+++ app.R
@@ -104,7 +104,11 @@ ui <- navbarPage(theme = shinytheme("yeti"),
                                 includeCSS("V:/lum/WM&S/BEAR (Bureau of Environmental Analysis and Restoration)/Envpln/Hourly Employees/KevinZolea/4tribinfo/Del_Tribs_Project/del_tribs_app/www/styles.css")),
                               h1(""),
                               h2("Introduction:"),
-                              h3("The purpose of this app is to help guide decision making for the modeling efforts in the Delaware Tributaries Project."),
+                              h3("The purpose of this app is to help guide decision making for the modeling efforts in the Delaware Tributaries Project. It
+                                 will give users the ability to understand the current data we have and the monitoring stations where the data is collected. This in turn
+                                 will provide useful information for the modelers of this project. The models developed through this project
+                                 will provide the useful loading information from upstream nontidal portions of the waterbodies into the tidal
+                                 portions and will allow development of site-specifc criteria if necessary."),
                               br(),
                               HTML('<center><img src="http://media.nj.com/centraljersey_impact/photo/9558763-large.jpg"></center>')),
                           tags$head(tags$script(HTML('
